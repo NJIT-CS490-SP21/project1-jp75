272caa441031fb5242e38031bacf29ca127bd01c
EC2 Default User <ec2-user@ip-172-31-53-194.ec2.internal>
2021-02-10 16:51:55 +0000

    updated styles and added lines that explained code

--- genius_api.py
+++ genius_api.py
@@ -11,11 +11,12 @@ headers = {
     'Authorization': 'Bearer ' + genius_token
 }
     
+
 def getLyrics(info):
-    name_song = info[0]
-    name_artist = info[1]
+    name_song = info[0]                                 #name of the song from list
+    name_artist = info[1]                               #name of the artist from list
     url = 'https://api.genius.com/search'
-    data = {'q': name_song + ' ' + name_artist}
-    response = requests.get(url, data = data, headers = headers).json()['response']['hits'][0]['result']['url']
+    data = {'q': name_song + ' ' + name_artist}         #genius api uses q=Kendrick%20Lamar 
+    response = requests.get(url, data = data, headers = headers).json()['response']['hits'][0]['result']['url']     #parses lyrics url
     return response
 
