6c19482744b872b36e729abc204f619449d1f114
EC2 Default User <ec2-user@ip-172-31-53-194.ec2.internal>
2021-02-10 17:01:25 +0000

    attempt to fix bug

--- app.py
+++ app.py
@@ -10,13 +10,13 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index():
     artist_IDs = ['74XFHRwlV6OrjEM0A2NCMF', #paramore
     '6XyY86QOPPrYVGvF9ch6wz',            #Linkin Park
-    '3TVXtAsR1Inumwj472S9r4' ,           #Drake
+    '3TVXtAsR1Inumwj472S9r4'            #Drake
     ]
     
     artist_id = random.choice(artist_IDs)       #picks a random artist
 
-    random_track = topTracks(artist_id)         #gets the information from spotify api def Toptracks()
-    Top_info = track_info(random_track)         #parses info of the Toptracks() in Track_info()
+    random_track = topTracks(artist_id)         #gets the information from spotify api def Toptracks
+    Top_info = track_info(random_track)         #parses info of the Toptracks in Track_info
     Top_info = random.choice(Top_info)          #choses a random song and returns the info from it
     info = Top_info.split(',')                  #splits info into song name, artist name, album url, preview url
     
